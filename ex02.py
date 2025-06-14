7a21cd015bb6f44781d86a6334e3f6f2d5653c5b
MiguelP <77172963+MiguelPerino@users.noreply.github.com>
2025-06-13 22:03:07 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,10 +1,20 @@
 def segundos(h:int, m: int, s: int) -> int:
-    if h < 0:
-        print('Apenas números inteiros e positivos.')
+    if h < 0 or m < 0 or s < 0:
+        raise ValueError ('Erro! Apenas números inteiros e positivos!')
     if h >= 0:
         horas = h * 60 *60
     if m >= 0:
         minutos = m * 60
     soma = horas + minutos + s
-    print(soma)
-print(segundos(2, 40, 10))  
+    print(f'O tempo total em segundos é igual a: {soma}')
+
+try:
+    hour = int(input('Informe a hora: '))
+    minute = int(input('Informe os minutos: '))
+    seconds = int(input('Informe os segundos: '))
+    segundos(hour, minute, seconds)
+except ValueError as e:
+    print(f'Erro: {e}')
+
+
+input('\nPressione Enter para fechar o programa...')
